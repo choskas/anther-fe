a50e0f6c635981aba518e1e9bc75737d0f300a57
bichous <bichous@users.noreply.github.com>
2024-06-04 10:36:07 -0600

    react proxy added

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,15 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { serveStatic } from 'hono/bun'
 import { studentsRoute } from './routes/students'
 
 const app = new Hono()
 
 app.use('*', logger())
 
-app.get('/test', (c) => {
-  return c.json({ message: 'test' })
-})
-
 app.route('/api/students', studentsRoute)
+
+app.get('*', serveStatic({ root: './frontend/dist' }))
+app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 export default app
